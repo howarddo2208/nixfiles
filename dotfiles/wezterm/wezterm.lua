b0dcc329fd07612e8101f36c217caeb2ae4a923d
Howard Do <howarddo2208@gmail.com>
2023-05-10 17:25:50 +0700

    migrate some existing dotfiles over

---
+++ dotfiles/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'JetBrainsMono Nerd Font Mono'
+
+config.color_scheme = 'GitHub Dark'
+config.window_background_opacity = 0.9
+
+return config
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, pkgs, libs, ... }: {
   home.username = "tanhopdo";
   home.homeDirectory = "/home/tanhopdo";
   home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
@@ -23,8 +23,39 @@
     pkgs.fnm
     pkgs.bat
     pkgs.lazygit
+    pkgs.karabiner-elements
   ];
 
-  # Install the gitconfig file, as .gitconfig in the home directory
-  # home.file.".config/wezterm".source = ./gitconfig;
+  # TODO install wezterm without compile error
+  #  programs.wezterm = {
+  #   enable = true;
+  #   extraConfig = ''
+  #     local wezterm = require 'wezterm'
+  #     return {
+  #       font = wezterm.font 'JetBrainsMono Nerd Font Mono'
+  #       color_scheme = 'GitHub Dark'
+  #       window_background_opacity = 0.9
+  #     }
+  #   '';
+  #  };
+
+  # symlink dotfiles, ensure that any changes while running programs are not lost
+  home.file = {
+    ".config/karabiner" = {
+      source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/karabiner;
+      recursive = true;
+    };
+    ".config/nvim" = {
+      source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/nvim;
+      recursive = true;
+    };
+    ".config/mc" = {
+      source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/mc;
+      recursive = true;
+    };
+    ".config/wezterm" = {
+      source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/wezterm;
+      recursive = true;
+    };
+  };
 }
