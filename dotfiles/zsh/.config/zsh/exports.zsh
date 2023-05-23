edb860e6e571acfd6d198c90689bc2b4e8ab88da
Howard Do <howarddo2208@gmail.com>
2023-05-23 18:21:27 +0700

    add catppuccin for lazygit

--- dotfiles/zsh/.config/zsh/exports.zsh
+++ dotfiles/zsh/.config/zsh/exports.zsh
@@ -2,6 +2,7 @@
 # HISTFILE="$XDG_DATA_HOME"/zsh/history
 HISTSIZE=1000000
 SAVEHIST=1000000
+export XDG_CONFIG_HOME="$HOME/.config"
 export EDITOR="nvim"
 export PATH="$HOME/.local/bin":$PATH
 export PNPM_HOME="$HOME/.local/share/pnpm"
