5eb6c8889ade8da234adea6083e79fc9992abb8c
Vincent.Vu <Vincent.Vu@aia.com>
2023-10-05 09:52:59 +0700

    update

--- dotfiles/nvim/.config/nvim/after/plugin/typescript-tools.lua
+++ dotfiles/nvim/.config/nvim/after/plugin/typescript-tools.lua
@@ -1,3 +1,7 @@
+if not pcall(require, 'typescript-tools') then
+  return
+end
+
 require("typescript-tools").setup {
   settings = {
     tsserver_plugins = {
@@ -8,3 +12,26 @@ require("typescript-tools").setup {
     },
   },
 }
+
+local wk = require("which-key")
+local function createBindKeyOpts(mode)
+  return {
+    mode = mode,
+    noremap = true,
+    silent = true,
+    noawait = true,
+  }
+end
+local normalModeOpts = createBindKeyOpts("n")
+
+wk.register({
+  ["<leader>T"] = {
+    name = 'Typescript+',
+    f = { ':TSToolsFixAll<CR>', "fix all" },
+    s = { ':TSToolsSortImports<CR>', "sort imports" },
+    r = { ':TSToolsRemoveUnusedImports<CR>', "remove unused imports" },
+    R = { ':TSToolsRemoveUnused<CR>', "remove unused statements" },
+    a = { ':TSToolsAddMissingImports<CR>', "add missign imports" },
+    d = { ':TSToolsGoToSourceDefinition<CR>', "go to source definition" },
+  },
+})
